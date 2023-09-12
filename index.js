469bd8fb052eae55d0b5db4dc59b87134f8e0629
nicolas572 <nicolas572@yahoo.com>
2023-09-11 17:35:03 -0700

    completed activity

--- index.js
+++ index.js
@@ -16,18 +16,20 @@ let globalStore = {}
 // function for checking a password
 checkPassword = async (username, plaintextPassword) => {
     // TODO: Make sure to delete this console.log once you're done implementing the function!
-    console.log('\n Uh-oh, checkPassword is not yet implemented. 😢')
+    // console.log('\n Uh-oh, checkPassword is not yet implemented. 😢')
     // Ensure global store contains the user 
     // (this is a quick way to check if an object contains a key)
     if (globalStore[username]) {
-        // TODO: Use bcrypt's compare methof to compare a plaintext password to a password hash
-
+        // TODO: Use bcrypt's compare method to compare a plaintext password to a password hash
+        let result = await bcrypt.compare(plaintextPassword, globalStore[username])
         // TODO: The result variable is a boolean. True means the user was valid. Take action accordingly.
         if (result) {
             // TODO: Display message for valid credentials
+            console.log('\n✅ Login successful, ${username}!\n')
         }
         else {
             // TODO: Display message for invalid credentials
+            console.log('\n❌ Login failed, try again.\n')
         }
     }
     else {
@@ -38,13 +40,14 @@ checkPassword = async (username, plaintextPassword) => {
 
 hashPassword = async (username, password) => {
     // TODO: Make sure to delete this console.log once you're done implementing the function!
-    console.log('\nUh-oh, hashPassword is not yet implemented. 😢')
+    // console.log('\nUh-oh, hashPassword is not yet implemented. 😢')
 
     // TODO: Make the password hash using bcrypt
-
+    let hashedPassword = await bcrypt.hash(password, 12)
     // TODO: Add the user and password hash to the global store object
-
+    globalStore[username] = hashedPassword
     // TODO: Print a status update including the username and password hash
+    console.log(`\n✅ ${username} has been added to the system! Their password hash is: ${hashedPassword}\n`)
 }
 
 
